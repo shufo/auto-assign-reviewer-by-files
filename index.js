d78bce60b557d98bae5568ddf691d2547dc5c200
shufo <meikyowise@gmail.com>
2022-01-18 12:16:05 +0900

    fix: 🐛 team does not assigned as reviewers

--- index.js
+++ index.js
@@ -102,6 +102,7 @@ async function assignReviewers(octokit, reviewers) {
       repo: context.repo.repo,
       pull_number: context.payload.pull_request.number,
       reviewers: [reviewer],
+      team_reviewers: [reviewers],
     });
   }
 }
