3ab32926a97a85395edb3bf97c77801e37c3d805
Shuhei Hayashibara <meikyowise@gmail.com>
2023-08-11 15:59:15 +0900

    test: 💍 add test for matching

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,3 +5,10 @@ test("glob pattern test with dot files #23", async () => {
   expect(matched).toBeTruthy();
 });
 
+test("glob pattern test with dot files #23", async () => {
+  const matched = hasGlobPatternMatchedFile(
+    [".github/auto-assin.yml"],
+    "**/*.*"
+  );
+  expect(matched).toBeTruthy();
+});
